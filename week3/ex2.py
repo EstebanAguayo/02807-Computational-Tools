12e3703fe4a29c4bc26bc95b325724b966526854
EstebanAguayo <s213162@dtu.dk>
2022-09-23 14:20:38 +0200

    ex2 finished

--- week3/ex2.py
+++ week3/ex2.py
@@ -1,12 +1,24 @@
 from gettext import npgettext
 import numpy as np
 from scipy import interpolate
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    list = np.loadtxt("data2.txt")
+    #x=[1,2,3,4]
+    #np.savetxt("data3.txt",x)
+    list = np.loadtxt("week3/data2.txt")
     print(list)
-    #x = list[:,0]
-    #y = list[:,1]
+    x = list[:,0]
+    y = list[:,1]
 
-    #pol = interpolate.interp1d(x,y)
-    #print(pol)
+    f = interpolate.interp1d(x,y, kind='cubic')
+
+    y1=f(x)
+
+    #plt.figure(1)
+    plt.plot(x,y,'r')
+    plt.plot(x,y1,'x')
+    plt.legend(["real", "interpolated"])
+    plt.show()
+
+    
