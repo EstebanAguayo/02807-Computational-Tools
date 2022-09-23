a79429ec803c37e46838e49e552ff1324e60e828
EstebanAguayo <s213162@dtu.dk>
2022-09-23 15:29:55 +0200

    ex3 done

---
+++ week3/ex3.py
@@ -0,0 +1,39 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from numba import njit
+import time
+
+
+start_time = time.time()
+
+def sum(n):
+    res = 0
+    for i in range(1, n):
+        res += 1/(i**2)
+    return res
+
+@njit
+def numbaSum(n):
+    res = 0
+    for i in range(1, n):
+        res += 1/(i**2)
+    return res
+
+if __name__ == "__main__":
+    start = time.perf_counter()                         #Time start
+
+    n = int(sys.argv[1])                                   # The first argument, sys.argv[0] is the name of the file...
+    sel = sys.argv[2]
+
+    iter = 1
+
+    if sel == 1:
+        for i in range(iter):
+            res = sum(n)
+    else:
+        for i in range(iter):
+            res = numbaSum(n)
+
+    end = time.perf_counter()
+    print("Standard way result %s, time %.7f" %(res, end-start))
