bd98e54e94a7da59e068f88bc432e9fcf4f1125a
EstebanAguayo <s23162@dtu.dk>
2022-11-06 19:05:23 +0100

    commit

--- Project/wiki_scraping.py
+++ Project/wiki_scraping.py
@@ -3,20 +3,38 @@ import wikipedia as wiki
 import os #For data files extraction from folder
 
 
-'''file_loc = os.getcwd()
+exception_list = []
+file_loc = os.getcwd()
+
+#exclude_list = ["Abdus Salam","Abiy Ahmed Ali","Akira Suzuki","Al Gore"] 
 
 df = pd.read_csv(file_loc+"\\Project\\Data\\SourceKaggle\\complete.csv")
 df_ind=df[df["ind_or_org"]=="Individual"]
 people_names = df_ind['name']
-#people_names[people_names == "abdul salam"].index[0]
-
+print(len(people_names))
+#people_names.drop("Abdus Salam")
 
 #people_names.drop([people_names=="abdul salam"])
-
-for name in people_names:
-    page_data = wiki.page(name).content
-    notes_index = page_data.find("== Notes ==")
-    page_data = page_data[0:notes_index]
-    with open(file_loc+"\\Project\\Data\\NewWiki\\"+name+".txt", "w", encoding="utf-8") as text_file:
-        text_file.write(page_data)
-        text_file.close()'''
+while True:
+    for name in people_names:
+        try:
+            print(name)
+            page_data = wiki.page(name).content
+            notes_index = page_data.find("== Notes ==")
+            if notes_index == -1:
+                notes_index = page_data.find("== See also ==")
+            elif notes_index == -1:
+                notes_index = page_data.find("== References ==")
+            page_data = page_data[0:notes_index]
+            with open(file_loc+"\\Project\\Data\\NewWiki\\"+name+".txt", "w", encoding="utf-8") as text_file:
+                text_file.write(page_data)
+                text_file.close()
+        except wiki.exceptions.PageError:
+            print("Catched")
+            exception_list.append(name)
+            continue
+        except wiki.exceptions.DisambiguationError:
+            print("Catched")
+            exception_list.append(name)
+            continue
+    print(exception_list)
